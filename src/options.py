1e9e2c6febdf90bfadb52190fd4db82479d409ce
Ninad Akolekar <ninad.akolekar@gmail.com>
2019-07-09 15:20:07 +0900

    Update options.py

--- src/options.py
+++ src/options.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 import os
-import torch
 import argparse
 
 
@@ -17,7 +16,6 @@ class ModelOptions:
         parser.add_argument('--lr', type=float, default=0.001, metavar='LR', help='learning rate (default: 0.01)')
         parser.add_argument('--beta1', type=float, default=0.9, metavar='M', help='Adam beta1 (default: 0.9)')
         parser.add_argument('--beta2', type=float, default=0.999, metavar='M', help='Adam beta2 (default: 0.999)')
-        parser.add_argument('--no-cuda', action='store_true', default=False, help='disables CUDA training')
         parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
         parser.add_argument('--log-interval', type=int, default=50, metavar='N', help='how many batches to wait before logging training status')
         parser.add_argument('--gpu-ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
@@ -27,7 +25,6 @@ class ModelOptions:
     def parse(self):
         opt = self._parser.parse_args()
         os.environ['CUDA_VISIBLE_DEVICES'] = opt.gpu_ids
-        opt.cuda = not opt.no_cuda and torch.cuda.is_available()
         opt.debug = opt.debug != 0
 
         args = vars(opt)
