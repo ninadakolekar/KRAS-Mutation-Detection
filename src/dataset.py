f6825e52cd6ee1cd1184287fd2f04eee7b5653a8
Ninad Akolekar <ninad.akolekar@gmail.com>
2019-07-09 16:03:20 +0900

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -6,7 +6,7 @@ import numpy as np
 import cv2
 from PIL import Image
 
-from keras.utils.data_utils import Sequence
+from keras.utils import Sequence
 
 from albumentations import (
     Compose, HorizontalFlip, HueSaturationValue,
