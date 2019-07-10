993faffc04fd0c4bc1daec83e3f037a9c4600293
Ninad Akolekar <ninad.akolekar@gmail.com>
2019-07-10 15:48:37 +0900

    Create visualize.py

---
+++ src/visualize.py
@@ -0,0 +1,36 @@
+import os
+import sys
+from math import ceil
+
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+import tensorflow as tf
+from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard, ReduceLROnPlateau
+
+from options import TrainingOptions
+from dataset import CellColonySequence
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+
+base_model = InceptionV3(include_top=False, input_shape=(512,512,3), pooling='avg', classes=2)
+x = base_model.output
+x = Dense(32, activation='relu')(x)
+x = Dense(8, activation='relu')(x)
+model = Dense(2, activation='softmax')(x)
+
+model.load_weights(sys.argv[1])
+
+model.compile(loss=tf.keras.losses.categorical_crossentropy,optimizer='adam',metrics=['accuracy'])
+
+print("=== VALIDATION ===")
+val_gen = CellColonySequence("/home/nitish/Desktop/ninad/kras/data/data4/valid",512,1,augmentations=None)
+print(model.evaluate_generator(val_gen))
+
+print("=== TEST ===")
+test_gen = CellColonySequence("/home/nitish/Desktop/ninad/kras/data/data4/test",512,1,augmentations=None)
+print(model.evaluate_generator(test_gen))
+
