53bacff4b79e61c8fb4adf2058de7fdaef0f6052
Ninad Akolekar <ninad.akolekar@gmail.com>
2019-07-10 19:35:43 +0900

    a

--- src/dataset.py
+++ src/dataset.py
@@ -26,13 +26,60 @@ class CellColonySequence(Sequence):
         def filenames(index):
             if mode == 'valtest':
                 filenames = list(glob.glob(path + '/valid/' + LABELS[index] + '/*.JPG'))+list(glob.glob(path + '/test/' + LABELS[index] + '/*.JPG'))
-            else:
+            elif mode=='train':
                 filenames = list(glob.glob(path + '/' + LABELS[index] + '/*.JPG'))
             
             return filenames
 
         
         labels = {name: index for index in range(len(LABELS)) for name in filenames(index)}
+        l = list(labels.items())
+        labels = dict(l)
+
+        self.path = path
+        self.names = list(labels.keys())
+        self.labels = list(labels.values())
+        self.input_size = input_size
+        self.batch_size = batch_size
+
+        AUGMENTATIONS_TRAIN = Compose([
+            HorizontalFlip(p=0.5),
+            RandomContrast(limit=0.2, p=0.5),
+            RandomGamma(gamma_limit=(80, 120), p=0.5),
+            RandomBrightness(limit=0.2, p=0.5),
+            HueSaturationValue(hue_shift_limit=5, sat_shift_limit=20,
+                            val_shift_limit=10, p=.9),
+            RandomRotate90(),
+            Resize(input_size,input_size),
+            ToFloat(max_value=255)
+        ])
+
+        AUGMENTATIONS_TEST = Compose([
+            Resize(input_size,input_size),
+            ToFloat(max_value=255)
+        ])
+
+        self.augment = AUGMENTATIONS_TRAIN if augmentations == 'train' else AUGMENTATIONS_TEST
+
+    def __len__(self):
+
+        return int(np.ceil(len(self.names) / float(self.batch_size)))
+
+    def __getitem__(self, idx):
+
+        batch_x = self.names[idx * self.batch_size:(idx + 1) * self.batch_size]
+        batch_y = np_utils.to_categorical(self.labels[idx * self.batch_size:(idx + 1) * self.batch_size],num_classes=2)
+                
+        return (np.stack([self.augment(image=np.array(Image.open(name)))["image"] for name in batch_x], axis=0), np.array(batch_y))
+
+class CellColonyTestSequence(Sequence):
+
+    def __init__(self, path, input_size, batch_size, augmentations,mode='train'):
+
+        random.seed(42)
+
+        
+        labels = {name: index for index in range(1) for name in glob.glob(path + '/*.JPG')}
         l = list(labels.items())
         labels = dict(l)
 
