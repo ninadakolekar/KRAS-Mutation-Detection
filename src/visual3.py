f957aec951764a58b7a9363f479d7e476d3254ce
Ninad Akolekar <ninad.akolekar@gmail.com>
2019-07-10 20:16:09 +0900

    Update visual3.py

--- src/visual3.py
+++ src/visual3.py
@@ -34,52 +34,51 @@ model = Model(outputs=out,inputs=base_model.input)
 model = load_model("/home/nitish/Desktop/ninad/kras/code/kras-keras-old/output/s512_20190710-144144/ic_model.h5")
 
 model.compile(loss=tf.keras.losses.categorical_crossentropy,optimizer='adam',metrics=['accuracy'])
-classifier = model
-layer_outputs = [layer.output for layer in classifier.layers[:NUM_LAYERS]] 
-# Extracts the outputs of the top 12 layers
-activation_model = Model(inputs=classifier.input, outputs=layer_outputs) # Creates a model that will return these outputs, given the model input
+
+complete_model = model
+
+layer_outputs = [layer.output for layer in complete_model.layers[:50]]
 
 img_path = random.choice(os.listdir("/home/nitish/Desktop/ninad/kras/data/data4/test/kras"))
-img_path = os.path.join("/home/nitish/Desktop/ninad/kras/data/data4/test/kras",img_path)
+test_image = os.path.join("/home/nitish/Desktop/ninad/kras/data/data4/test/kras",img_path)
 
-img = image.load_img(img_path, target_size=(512,512,3))
+print(f"Name: {test_image}")
+
+img = image.load_img(test_image, target_size=(512,512,3))
 img_tensor = image.img_to_array(img)
 img_tensor = np.expand_dims(img_tensor, axis=0)
 img_tensor /= 255.
-images_per_row = 16
 
-layer_names = []
-for layer in classifier.layers[:NUM_LAYERS]:
-    layer_names.append(layer.name) # Names of the layers, so you can have them as part of your plot
+print(model.summary())
+exit(0)
 
+activation_model = Model(inputs=complete_model.input, outputs=layer_outputs)
 activations = activation_model.predict(img_tensor)
 
-for layer_name, layer_activation in zip(layer_names, activations): # Displays the feature maps
-    n_features = layer_activation.shape[-1] # Number of features in the feature map
-    size = layer_activation.shape[1] #The feature map has shape (1, size, size, n_features).
-    n_cols = n_features // images_per_row # Tiles the activation channels in this matrix
+layer_names = ['conv2d_1', 'activation_1', 'conv2d_4', 'activation_4', 'conv2d_9', 'activation_9']
+activ_list = [activations[1], activations[3], activations[11], activations[13], activations[18], activations[20]]
+
+images_per_row = 16
+
+for layer_name, layer_activation in zip(layer_names, activ_list):
+    n_features = layer_activation.shape[-1]
+    size = layer_activation.shape[1]
+    n_cols = n_features // images_per_row
     display_grid = np.zeros((size * n_cols, images_per_row * size))
-    for col in range(n_cols): # Tiles each filter into a big horizontal grid
+    
+    for col in range(n_cols):
         for row in range(images_per_row):
-            channel_image = layer_activation[0,
-                                             :, :,
-                                             col * images_per_row + row]
-            channel_image -= channel_image.mean() # Post-processes the feature to make it visually palatable
+            channel_image = layer_activation[0, :, :, col * images_per_row + row]
+            channel_image -= channel_image.mean()
             channel_image /= channel_image.std()
             channel_image *= 64
             channel_image += 128
             channel_image = np.clip(channel_image, 0, 255).astype('uint8')
-            display_grid[col * size : (col + 1) * size, # Displays the grid
-                         row * size : (row + 1) * size] = channel_image
+            display_grid[col * size : (col + 1) * size, row * size : (row + 1) * size] = channel_image
+
     scale = 1. / size
-    plt.figure(figsize=(scale * display_grid.shape[1],
-                        scale * display_grid.shape[0]))
+    plt.figure(figsize=(scale * display_grid.shape[1], scale * display_grid.shape[0]))
     plt.title(layer_name)
     plt.grid(False)
-    plt.imshow(display_grid, aspect='auto', cmap='viridis')
-    
-    plt.savefig(f"{layer_name}.png")
-    plt.close()
-
-
-
+    plt.imshow(display_grid, aspect='auto', cmap='plasma')
+    plt.savefig(layer_name+"_grid.jpg", bbox_inches='tight')
