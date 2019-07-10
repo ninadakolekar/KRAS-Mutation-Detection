006c738ffcc2cd503178309799b6bbd7e70420fd
Ninad Akolekar <ninad.akolekar@gmail.com>
2019-07-10 14:00:46 +0900

    Update model.py

--- src/model.py
+++ src/model.py
@@ -32,16 +32,16 @@ if __name__ == "__main__":
     base_model = get_base_model(args.input_size)
     x = base_model.output
     x = Dropout(0.4)(x)
-    # x = Dense(8, activation='relu')(x)
-    # x = Dense(4, activation='relu')(x)
+    x = Dense(32, activation='relu')(x)
+    x = Dense(8, activation='relu')(x)
     predictions = Dense(2, activation='softmax')(x)
     
-    model = Model(inputs=base_model.input, outputs=predictions)
-    # model = multi_gpu_model(Model(inputs=base_model.input, outputs=predictions),gpus=2)
+    # model = Model(inputs=base_model.input, outputs=predictions)
+    model = multi_gpu_model(Model(inputs=base_model.input, outputs=predictions),gpus=2)
 
     model.compile(loss=tf.keras.losses.categorical_crossentropy,optimizer='adam',metrics=['accuracy'])
 
-    history = model.fit_generator(train_gen,steps_per_epoch=10*ceil(train_gen.__len__()/args.batch_size),epochs=args.epochs,callbacks=callbacks,validation_data=val_gen,validation_steps=ceil(val_gen.__len__()/args.batch_size),workers=1,use_multiprocessing=False,shuffle=True)
+    history = model.fit_generator(train_gen,steps_per_epoch=10*ceil(train_gen.__len__()),epochs=args.epochs,callbacks=callbacks,validation_data=val_gen,validation_steps=ceil(val_gen.__len__()),workers=1,use_multiprocessing=False,shuffle=True)
 
     model.save(os.path.join(args.outdir,"model.h5"))
 
@@ -64,3 +64,6 @@ if __name__ == "__main__":
     plt.xlabel('epoch')
     plt.legend(loc='upper left')
     plt.savefig(os.path.join(args.outdir,"loss.png"))
+
+    # test_gen = CellColonySequence(os.path.join(args.dataset_path,"valid"),args.input_size,args.batch_size,augmentations=None)
+    # model.evaluate_generator(test_gen)
