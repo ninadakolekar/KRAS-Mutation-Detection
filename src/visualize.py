af5a76eb00f62f9c162f71f98c7d6da1eb6cac58
Ninad Akolekar <ninad.akolekar@gmail.com>
2019-07-10 17:05:01 +0900

    Update visualize.py

--- src/visualize.py
+++ src/visualize.py
@@ -26,38 +26,40 @@ model = Model(outputs=out,inputs=base_model.input)
 
 model = load_model(sys.argv[1])
 
-print(model.layers)
-
 model.compile(loss=tf.keras.losses.categorical_crossentropy,optimizer='adam',metrics=['accuracy'])
 
-print(f"Metrics: {model.metrics_names}")
+# print(f"Metrics: {model.metrics_names}")
 
-print("=== TRAIN ===")
-train_gen = CellColonySequence("/home/nitish/Desktop/ninad/kras/data/data4/train",512,1,augmentations=None)
-print(model.evaluate_generator(train_gen))
+# print("=== TRAIN ===")
+# train_gen = CellColonySequence("/home/nitish/Desktop/ninad/kras/data/data4/train",512,1,augmentations=None)
+# print(model.evaluate_generator(train_gen))
 
-predictions = model.predict_generator(train_gen).argmax(axis=1)
-unique, counts = np.unique(predictions, return_counts=True)
-print(dict(zip(unique, counts)))
+# predictions = model.predict_generator(train_gen).argmax(axis=1)
+# unique, counts = np.unique(predictions, return_counts=True)
+# print(dict(zip(unique, counts)))
 
-del train_gen
+# del train_gen
 
-print("=== VALIDATION ===")
-val_gen = CellColonySequence("/home/nitish/Desktop/ninad/kras/data/data4/valid",512,1,augmentations=None)
-print(model.evaluate_generator(val_gen))
+# print("=== VALIDATION ===")
+# val_gen = CellColonySequence("/home/nitish/Desktop/ninad/kras/data/data4/valid",512,1,augmentations=None)
+# print(model.evaluate_generator(val_gen))
 
-predictions = model.predict_generator(val_gen).argmax(axis=1)
-unique, counts = np.unique(predictions, return_counts=True)
-print(dict(zip(unique, counts)))
+# predictions = model.predict_generator(val_gen).argmax(axis=1)
+# unique, counts = np.unique(predictions, return_counts=True)
+# print(dict(zip(unique, counts)))
 
-del val_gen
+# del val_gen
 
-print("=== TEST ===")
+# print("=== TEST ===")
 test_gen = CellColonySequence("/home/nitish/Desktop/ninad/kras/data/data4/test",512,1,augmentations=None)
-print(model.evaluate_generator(test_gen))
+# print(model.evaluate_generator(test_gen))
 
-predictions = model.predict_generator(test_gen).argmax(axis=1)
-unique, counts = np.unique(predictions, return_counts=True)
-print(dict(zip(unique, counts)))
+# predictions = model.predict_generator(test_gen).argmax(axis=1)
+# unique, counts = np.unique(predictions, return_counts=True)
+# print(dict(zip(unique, counts)))
 
-del test_gen
+# del test_gen
+
+new_model = Model(inputs=model.input,outputs=model.layers[3].output)
+preds = new_model.predict_generator(test_gen)
+print(f"Shape: {preds.shape}")
