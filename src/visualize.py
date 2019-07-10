ea85538dd6fea39dc97d5185619d2e2d995854c4
Ninad Akolekar <ninad.akolekar@gmail.com>
2019-07-10 18:21:06 +0900

    Update visualize.py

--- src/visualize.py
+++ src/visualize.py
@@ -17,6 +17,7 @@ from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.applications.inception_v3 import InceptionV3
 from tensorflow.keras.preprocessing import image
+import tensorflow.keras.backend as K
 
 random.seed(42)
 
@@ -75,36 +76,49 @@ img_tensor = image.img_to_array(img)
 img_tensor = np.expand_dims(img_tensor, axis=0)
 img_tensor /= 255.
 
-activation_model = Model(inputs=complete_model.input, outputs=layer_outputs)
-activations = activation_model.predict(img_tensor)
-
-layer_names = ['conv2d_1', 'activation_1', 'conv2d_4', 'activation_4', 'conv2d_9', 'activation_9']
-activ_list = [activations[1], activations[3], activations[11], activations[13], activations[18], activations[20]]
-
-images_per_row = 16
-
-for layer_name, layer_activation in zip(layer_names, activ_list):
-    n_features = layer_activation.shape[-1]
-    size = layer_activation.shape[1]
-    n_cols = n_features // images_per_row
-    display_grid = np.zeros((size * n_cols, images_per_row * size))
+def deprocess_image(x):
     
-    for col in range(n_cols):
-        for row in range(images_per_row):
-            channel_image = layer_activation[0, :, :, col * images_per_row + row]
-            channel_image -= channel_image.mean()
-            channel_image /= channel_image.std()
-            channel_image *= 64
-            channel_image += 128
-            channel_image = np.clip(channel_image, 0, 255).astype('uint8')
-            display_grid[col * size : (col + 1) * size, row * size : (row + 1) * size] = channel_image
+    x -= x.mean()
+    x /= (x.std() + 1e-5)
+    x *= 0.1
+    x += 0.5
+    x = np.clip(x, 0, 1)
+    x *= 255
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
 
-    scale = 1. / size
-    plt.figure(figsize=(scale * display_grid.shape[1], scale * display_grid.shape[0]))
-    plt.title(layer_name)
-    plt.grid(False)
-    # plt.imshow(display_grid, aspect='auto', cmap='plasma')
-    plt.savefig(layer_name+"_grid.jpg", bbox_inches='tight')
-    plt.close()
+def generate_pattern(layer_name, filter_index, size=150):
+    
+    layer_output = model.get_layer(layer_name).output
+    loss = K.mean(layer_output[:, :, :, filter_index])
+    grads = K.gradients(loss, model.input)[0]
+    grads /= (K.sqrt(K.mean(K.square(grads))) + 1e-5)
+    iterate = K.function([model.input], [loss, grads])
+    input_img_data = np.random.random((1, size, size, 3)) * 20 + 128.
+    step = 1.
+    for i in range(80):
+        loss_value, grads_value = iterate([input_img_data])
+        input_img_data += grads_value * step
+        
+    img = input_img_data[0]
+    return deprocess_image(img)
+
+layer_name = 'conv2d_4'
+size = 512
+margin = 5
+results = np.zeros((8 * size + 7 * margin, 8 * size + 7 * margin, 3))
+
+for i in range(8):
+    for j in range(8):
+        filter_img = generate_pattern(layer_name, i + (j * 8), size=size)
+        horizontal_start = i * size + i * margin
+        horizontal_end = horizontal_start + size
+        vertical_start = j * size + j * margin
+        vertical_end = vertical_start + size
+        results[horizontal_start: horizontal_end, vertical_start: vertical_end, :] = filter_img
+        
+plt.figure(figsize=(20, 20))
+plt.savefig(results)
+
 
 
