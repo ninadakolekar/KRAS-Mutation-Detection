d1fa0089a2efcba68017f3e93c2a11bacdf01ba9
Ninad Akolekar <ninad.akolekar@gmail.com>
2019-07-10 17:28:19 +0900

    Update visualize.py

--- src/visualize.py
+++ src/visualize.py
@@ -15,6 +15,7 @@ from dataset import CellColonySequence
 from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.preprocessing import image
 
 base_model = InceptionV3(include_top=False, input_shape=(512,512,3), pooling='avg', classes=2)
 x = base_model.output
@@ -60,10 +61,46 @@ model.compile(loss=tf.keras.losses.categorical_crossentropy,optimizer='adam',met
 
 # del test_gen
 
-print(isinstance(model.layers[3],Model))
-import pdb; pdb.set_trace()
+complete_model = model.layers[3]
 
-test_gen = CellColonySequence("/home/nitish/Desktop/ninad/kras/data/data4/test",512,1,augmentations=None)
-new_model = Model(inputs=model.input,outputs=model.layers[3].output)
-preds = new_model.predict_generator(test_gen)
-print(f"Shape: {preds.shape}")
+layer_outputs = [layer.output for layer in complete_model.layers[:50]]
+test_image = 'any test image'
+
+img = image.load_img(test_image, target_size=(512,512))
+img_tensor = image.img_to_array(img)
+img_tensor = np.expand_dims(img_tensor, axis=0)
+img_tensor /= 255.
+
+activation_model = Model(inputs=complete_model.input, outputs=layer_outputs)
+activations = activation_model.predict(img_tensor)
+
+layer_names = ['conv2d_1', 'activation_1', 'conv2d_4', 'activation_4', 'conv2d_9', 'activation_9']
+activ_list = [activations[1], activations[3], activations[11], activations[13], activations[18], activations[20]]
+
+images_per_row = 16
+
+for layer_name, layer_activation in zip(layer_names, activ_list):
+    n_features = layer_activation.shape[-1]
+    size = layer_activation.shape[1]
+    n_cols = n_features // images_per_row
+    display_grid = np.zeros((size * n_cols, images_per_row * size))
+    
+    for col in range(n_cols):
+        for row in range(images_per_row):
+            channel_image = layer_activation[0, :, :, col * images_per_row + row]
+            channel_image -= channel_image.mean()
+            channel_image /= channel_image.std()
+            channel_image *= 64
+            channel_image += 128
+            channel_image = np.clip(channel_image, 0, 255).astype('uint8')
+            display_grid[col * size : (col + 1) * size, row * size : (row + 1) * size] = channel_image
+
+    scale = 1. / size
+    plt.figure(figsize=(scale * display_grid.shape[1], scale * display_grid.shape[0]))
+    plt.title(layer_name)
+    plt.grid(False)
+    # plt.imshow(display_grid, aspect='auto', cmap='plasma')
+    plt.savefig(layer_name+"_grid.jpg", bbox_inches='tight')
+    plt.close()
+
+
